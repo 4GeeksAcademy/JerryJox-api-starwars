21ac597db118f48492865f77a84d4b21586bdc65
Luis Lugo <124685188+JerryJox@users.noreply.github.com>
2023-05-26 19:27:04 +0000

    upload files

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, Address
+from models import db, Films
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -10,8 +10,8 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Address, db.session))
+    admin.add_view(ModelView(Films, db.session))
+    # admin.add_view(ModelView(Address, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -2,14 +2,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+# CÓDIGO DE 
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.id
+
+class Films(db.Model):
+    id_films = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    characters = db.Column(db.String(120), unique=True, nullable=False)
+    starships = db.Column(db.String(120), unique=True, nullable=False)
+    planets = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.id
+        return '<Films %r>' % self.id
 
 # <User 1> => {
         #     "id": self.id,
@@ -18,24 +29,26 @@ class User(db.Model):
         # }
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "id_films": self.id,
+            "name": self.email,
+            "character": self.character,
+            "starships": self.starships,
+            "planets": self.planets,
         }
 
-class Address(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    calle = db.Column(db.String(120), unique=True, nullable=False)
-    numero = db.Column(db.String(80), unique=False, nullable=False)
+# class Address(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     calle = db.Column(db.String(120), unique=True, nullable=False)
+#     numero = db.Column(db.String(80), unique=False, nullable=False)
 
 
-    def __repr__(self):
-        return '<Address %r>' % self.id
+#     def __repr__(self):
+#         return '<Address %r>' % self.id
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "calle": self.calle,
-            "numero": self.numero
-            # do not serialize the password, its a security breach
-        }
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "calle": self.calle,
+#             "numero": self.numero
+#             # do not serialize the password, its a security breach
+#         }
